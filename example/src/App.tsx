eb7910bb1dca87d8bc8018452860d7be7708b97c
张家乐 <zhangjiale@zhangjialedeiMac.local>
2017-06-11 07:28:50 +0800

    删除一些文件

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -4,7 +4,7 @@ import HlsPlayer from '../../'
 
 class App extends React.Component<any, any>{
     render() {
-        return <HlsPlayer autoPlay controls src="http://184.72.239.149/vod/smil:BigBuckBunny.smil/playlist.m3u8" swf="/libs/flashls/bin/release/flashlsChromeless.swf"  />
+        return <HlsPlayer autoPlay controls src="http://184.72.239.149/vod/smil:BigBuckBunny.smil/playlist.m3u8" swf="/flashls/flashlsChromeless.swf"  />
     }
 }
 
